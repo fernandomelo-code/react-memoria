5837843f83718a53a53a0288865eb891f8d00c10
fernandomelo-code <melo_almeida@hotmail.com>
2022-10-24 11:07:36 -0400

    adding files to repo

---
+++ src/data/items.ts
@@ -0,0 +1,16 @@
+import android from '../svgs/android.svg';
+import caminhao  from '../svgs/caminhao.svg';
+import disney from '../svgs/disney.svg';
+import gasolina from '../svgs/gasolina.svg';
+import moto  from '../svgs/moto.svg';
+import estrela from '../svgs/estrela.svg';
+
+
+export const items = [
+    { name: 'android', icon: android },
+    { name: 'caminhao', icon: caminhao },
+    { name: 'disney', icon: disney },
+    { name: 'gasolina', icon: gasolina },
+    { name: 'moto', icon: moto },
+    { name: 'estrela', icon: estrela },    
+];
